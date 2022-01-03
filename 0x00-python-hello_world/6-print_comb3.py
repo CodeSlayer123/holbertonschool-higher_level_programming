c86c1cf5d304e5ae7f9200776ed41b742bcfcac0
CodeSlayer123 <3613@holbertonschool.com>
2022-01-03 13:02:48 -0800

    betty

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 flag = 0
-for i in range (0, 9):
-    print("{}".format(i), end = "")
+for i in range(0, 9):
+    print("{}".format(i), end="")
     if i == 8:
         flag = 1
-    for j in range (1, 10):
+    for j in range(1, 10):
         if flag == 1 and j == 9:
             print("{}".format(i))
         else:
-            print("{}".format(i), end = ",")
+            print("{}".format(i), end=",")
