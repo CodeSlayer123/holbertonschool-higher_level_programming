2631572781dcab7e641f886efc7569bfdefd9bd8
CodeSlayer123 <3613@holbertonschool.com>
2022-01-04 10:24:44 -0800

    0x01

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+flag = 0
+for i in range(0, 9):
+    print("{}".format(i), end="")
+    if i == 8:
+        flag = 1
+    for j in range(1, 10):
+        if flag == 1 and j == 9:
+            print("{}".format(i))
+        else:
+            print("{}".format(i), end=",")
