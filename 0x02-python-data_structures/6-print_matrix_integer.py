c9369778636b1d63ee5ef1ed14a31820d1dbcbbe
CodeSlayer123 <3613@holbertonschool.com>
2022-01-06 13:28:20 -0800

    :d

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,10 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         s = ' '.join(map(str, row))
-        print("{}".format(s))
+        for i in range(0, len(s), 2):
+            integer = int(s[i])
+            if i == len(s) - 1:
+                print("{:d}".format(integer), end="")
+            else:
+                print("{:d}".format(integer), end=" ")
+        print()
