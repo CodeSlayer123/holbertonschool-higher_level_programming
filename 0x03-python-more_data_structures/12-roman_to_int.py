29aeb41f000bd16322194cf808e900ac7b2541a8
CodeSlayer123 <3613@holbertonschool.com>
2022-01-10 14:40:33 -0800

    12b

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -8,8 +8,12 @@ def roman_to_int(roman_string):
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
     for i in roman_string:
+        if roman_string == 'I':
+            return(sum + 1)
         if count == 0 and i == 'I':
             sum -= romans[i]
+        elif roman_string == 'CM':
+            sum = romans[i] - sum
         else:
             sum += romans[i]
         count += 1
