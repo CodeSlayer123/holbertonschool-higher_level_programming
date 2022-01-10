0bb11a743b1243c530ad6509907720973dd27495
CodeSlayer123 <3613@holbertonschool.com>
2022-01-10 11:26:08 -0800

    6

---
+++ 0x03-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sort = dict(sorted(a_dictionary.items(), key=lambda item:item[0]))
+
+    for i in sort:
+        print("{}:".format(i,), sort[i])
