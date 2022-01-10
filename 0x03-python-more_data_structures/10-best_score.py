4463e7a30ffa9746657012dabb1269a453d55a75
CodeSlayer123 <3613@holbertonschool.com>
2022-01-10 13:06:18 -0800

    betty

--- 0x03-python-more_data_structures/10-best_score.py
+++ 0x03-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return(None)
     sort = dict(sorted(a_dictionary.items(), key=lambda item: item[1]))
     best = len(sort) - 1
