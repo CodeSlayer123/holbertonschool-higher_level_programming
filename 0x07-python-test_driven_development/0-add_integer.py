4ac35df76c47bd0a740789d94c9834b11d9955ff
Code Slayer <3613@holbertonschool.com>
2022-01-13 13:59:24 -0600

    Bettied .py, added docs

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Adds two integers"""
+
 
 def add_integer(a, b=98):
+    """Adds two integers, and converts to int if float"""
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
