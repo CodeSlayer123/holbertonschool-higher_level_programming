8fa9cbf1bf18a342c46f87c97dede6f6f96b6b91
Code Slayer <3613@holbertonschool.com>
2022-01-13 18:38:45 -0600

    test completed for matrix task

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,6 +4,7 @@
 
 def matrix_divided(matrix, div):
     """Divides numbers of a matrix rounded to 2 decimal places"""
+    # first batch of edgecases
     if type(matrix) != list:
             raise TypeError("matrix must be a matrix\
                 (list of lists) of integers/floats")
@@ -12,18 +13,26 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    new_matrix = [x[:] for x in matrix]
+    for row in matrix:  # check if variable matrix is a matrix
+        if type(row) != list:
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
     size = len(matrix[0])
 
-    for row in new_matrix:
-        if len(row) != size:
-            raise TypeError("Each row of the matrix\
-                            must have the same size")
+    if size == 0:  # check for empty matrix
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
+    for row in matrix:  # edgecases in matrix before copying
+        if len(row) != size:
+            raise TypeError("Each row of the matrix must have the same size")
         for i in range(len(row)):
             if type(i) != int and type(i) != float:
                 raise TypeError("matrix must be a matrix\
                     (list of lists) of integers/floats")
 
+    new_matrix = [x[:] for x in matrix]  # copy matrix
+
+    for row in new_matrix:  # editing my own matrix
+        for i in range(len(row)):
             row[i] = round(row[i] / div, 2)
     return(new_matrix)
