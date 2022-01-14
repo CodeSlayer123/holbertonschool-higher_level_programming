548daeed63a65c6a6ff2e530c9140d8cad06ef32
Code Slayer <3613@holbertonschool.com>
2022-01-14 13:02:22 -0600

    Changes width and height to be normal

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -6,8 +6,18 @@ class Rectangle:
     """Class that defines a rectangle by width and height"""
 
     def __init__(self, width=0, height=0):
-        self.__width = height
-        self.__height = width
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
