88d452c638c2a545bc5c8d859ba217a677cb2742
Code Slayer <3613@holbertonschool.com>
2022-01-13 19:11:08 -0600

    Update testing

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,8 +6,7 @@ def matrix_divided(matrix, div):
     """Divides numbers of a matrix rounded to 2 decimal places"""
     # first batch of edgecases
     if type(matrix) != list:
-            raise TypeError("matrix must be a matrix\
-                (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
     if div == 0:
@@ -27,8 +26,7 @@ def matrix_divided(matrix, div):
             raise TypeError("Each row of the matrix must have the same size")
         for i in range(len(row)):
             if type(i) != int and type(i) != float:
-                raise TypeError("matrix must be a matrix\
-                    (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     new_matrix = [x[:] for x in matrix]  # copy matrix
 
