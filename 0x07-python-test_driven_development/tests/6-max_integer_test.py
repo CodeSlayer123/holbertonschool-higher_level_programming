9c5f62e9f327899a01b147cdc6acc3e7a9b6df36
Code Slayer <3613@holbertonschool.com>
2022-01-17 12:11:05 -0600

    tests pass locally now

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,25 +1,25 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-class Test_max_integer(unittest.TestCase):
+class TestMaxInteger(unittest.TestCase):
 
     def test_max_at_end(self):
-        self.assertEqual([1, 2, 3, 4, 5], 5)
+        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
 
     def test_max_at_start(self):
-        self.assertEqual([5, 2, 3, 4, 1], 5)
+        self.assertEqual(max_integer([5, 2, 3, 4, 1]), 5)
 
     def test_max_at_mid(self):
-        self.assertEqual([1, 2, 5, 4, 3], 5)
+        self.assertEqual(max_integer([1, 2, 5, 4, 3]), 5)
 
     def test_one_neg(self):
-        self.assertEqual([1, 2, -3, 4, 5], 5)
+        self.assertEqual(max_integer([1, 2, -3, 4, 5]), 5)
 
     def test_all_negs(self):
-        self.assertEqual([-1, -2, -3, -4, -5], -1)
+        self.assertEqual(max_integer([-1, -2, -3, -4, -5]), -1)
 
     def test_one_element(self):
-        self.assertEqual([5], 5)
+        self.assertEqual(max_integer([5]), 5)
 
     def test_no_elements(self):
-        self.assertEqual([], 0)
+        self.assertEqual(max_integer([]), None)
