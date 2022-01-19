5618179d3fab87175d0e7eb82f9cc25a62867c99
Code Slayer <3613@holbertonschool.com>
2022-01-19 10:12:18 -0600

    betty

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,7 +6,8 @@ def matrix_divided(matrix, div):
     """Divides numbers of a matrix rounded to 2 decimal places"""
     # first batch of edgecases
     if type(matrix) != list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix \
+(list of lists) of integers/floats")
     if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
     if div == 0:
@@ -14,19 +15,23 @@ def matrix_divided(matrix, div):
 
     for row in matrix:  # check if variable matrix is a matrix
         if type(row) != list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix \
+(list of lists) of integers/floats")
 
     size = len(matrix[0])
 
     if size == 0:  # check for empty matrix
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix \
+(list of lists) of integers/floats")
 
     for row in matrix:  # edgecases in matrix before copying
         if len(row) != size:
-            raise TypeError("Each row of the matrix must have the same size")
+            raise TypeError("Each row of the matrix must \
+have the same size")
         for i in range(len(row)):
             if type(i) != int and type(i) != float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix \
+(list of lists) of integers/floats")
 
     new_matrix = [x[:] for x in matrix]  # copy matrix
 
