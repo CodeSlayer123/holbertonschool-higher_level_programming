e85c38f04bd23d1882321cddc9c0d31fa9cca6bd
Code Slayer <3613@holbertonschool.com>
2022-02-10 15:44:52 -0600

    gave task 10 a try

--- 0x01-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x01-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,1 +1,3 @@
 --  lists all shows in hbtn_0d_tvshows with at least one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres INNER JOIN tv_shows ON tv_show_genres.genre_id=tv_shows.genre_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
