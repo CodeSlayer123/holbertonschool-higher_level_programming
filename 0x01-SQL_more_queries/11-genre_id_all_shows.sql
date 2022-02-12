1f8c45c67eb508a45b7c472065f746340299e64a
Code Slayer <3613@holbertonschool.com>
2022-02-12 11:12:06 -0600

    added asc

--- 0x01-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x01-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,3 +1,3 @@
 -- lists all shows in database hbtn_0d_tvshows
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_shows.id=tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
