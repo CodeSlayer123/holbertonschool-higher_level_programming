d58bd29f59765bbe9285dc1c6766bad79261d21c
Code Slayer <3613@holbertonschool.com>
2022-02-14 11:23:47 -0600

    removed equal sign

--- 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- lists all cities of California in database hbtn_0d_usa
-SELECT id, name FROM cities WHERE states.name == 'California'
+SELECT id, name FROM cities WHERE states.name = 'California'
 ORDER BY cities.id ASC;
