40406340a50b27e483cf744ca319f5cca967a097
Code Slayer <3613@holbertonschool.com>
2022-02-24 11:10:12 -0600

    fixed git issues

---
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Lists all rows from the states table"""
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    mycursor = db.cursor()
+
+    sql = "SELECT * FROM states ORDER BY id ASC"
+    mycursor.execute(sql)
+    result = mycursor.fetchall()
+    for i in result:
+        print(i)
+
+    db.close()
---
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+db = MySQLdb.connect(host="localhost", port=3306,
+                     user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+mycursor = db.cursor()
+
+sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+
+mycursor.execute(sql)
+myresult = mycursor.fetchall()
+for i in myresult:
+    print(i)
+
+db.close()
