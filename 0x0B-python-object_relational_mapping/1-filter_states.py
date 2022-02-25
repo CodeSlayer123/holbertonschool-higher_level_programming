86b5e996c88b8761643841314ebb4ae5be8d889d
CodeSlayer123 <3613@holbertonschool.com>
2022-02-25 08:28:11 -0800

    case sensitive

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
     mycursor = db.cursor()
 
-    sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    sql = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
 
     mycursor.execute(sql)
     myresult = mycursor.fetchall()
