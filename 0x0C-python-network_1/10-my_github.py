a8e256f45dc57d20ab209e134784d478e6a335b5
Code Slayer <3613@holbertonschool.com>
2022-03-08 15:35:03 -0600

    changed import statement

--- 0x0C-python-network_1/10-my_github.py
+++ 0x0C-python-network_1/10-my_github.py
@@ -5,10 +5,11 @@ if __name__ == "__main__":
 
     import requests
     import sys
+    from requests.auth import HTTPBasicAuth
 
     user = sys.argv[1]
     pswd = sys.argv[2]
     url = f"https://api.github.com/users/{user}"
 
-    r = requests.get(url, auth=requests.auth.HTTPBasicAuth(user, pswd)).json()
+    r = requests.get(url, auth=HTTPBasicAuth(user, pswd)).json()
     print(r.get('id'))
