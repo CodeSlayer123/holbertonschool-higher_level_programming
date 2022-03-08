21a2fe472228e46c23a7d98743782093724e03a1
Code Slayer <3613@holbertonschool.com>
2022-03-08 15:33:10 -0600

    added http auth

--- 0x0C-python-network_1/10-my_github.py
+++ 0x0C-python-network_1/10-my_github.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
     import sys
 
     user = sys.argv[1]
+    pswd = sys.argv[2]
     url = f"https://api.github.com/users/{user}"
 
-    r = requests.get(url).json()
+    r = requests.get(url, auth=requests.auth.HTTPBasicAuth(user, pswd)).json()
     print(r.get('id'))
