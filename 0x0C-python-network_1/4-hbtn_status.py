ff470dd2da62411875a96e2cb7b84ec7f7c1f871
Code Slayer <3613@holbertonschool.com>
2022-03-08 13:17:53 -0600

    task 4 runs locally

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -2,10 +2,9 @@
 """fetches https://intranet.hbtn.io/status only using requests"""
 
 if __name__ == "__main__":
-    import urllib.request
+    import requests
 
     a = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
+    html = requests.get(a)
+    print("\t- type: {}".format(type(html.text)))
+    print("\t- content: {}".format(html.text))
