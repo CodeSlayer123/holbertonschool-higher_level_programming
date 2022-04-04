7fa408a2510b46b477ae1373124efd664f664bcf
Code Slayer <3613@holbertonschool.com>
2022-04-04 13:30:32 -0500

    task 8 runs locally

---
+++ 0x00-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const num = parseInt(process.argv[2]);
+if (isNaN(num)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j < num; j++) {
+      process.stdout.write('X');
+    }
+    process.stdout.write('\n');
+  }
+}
