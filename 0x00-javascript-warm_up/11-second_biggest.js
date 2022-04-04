9b08dd3b461b59d29d2d693f4389585544ece31f
Code Slayer <3613@holbertonschool.com>
2022-04-04 14:37:22 -0500

    semistandard

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 const numbers = [];
-if (!process.argv[2] || !process.argv[3]){
-    console.log(0)
+if (!process.argv[2] || !process.argv[3]) {
+  console.log(0);
+} else {
+  for (let i = 2; i < process.argv.length; i++) {
+    numbers[i - 2] = process.argv[i];
+  }
+  numbers.sort().reverse();
 
-} else{
-for (let i = 2; i < process.argv.length; i++) {
-  numbers[i - 2] = process.argv[i];
-}
-numbers.sort().reverse();
-
-console.log(numbers[1]);
+  console.log(numbers[1]);
 }
