c8751556839e33379682fed2d38b086edf7a06ea
Code Slayer <3613@holbertonschool.com>
2022-04-05 13:44:40 -0500

    inherited from square

--- 0x01-javascript_objects_scopes_closures/6-square.js
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const Squareb = require('./5-square.js');
 
-class Square extends Rectangle {
+class Square extends Squareb {
   constructor (size) {
     super(size, size);
   }
