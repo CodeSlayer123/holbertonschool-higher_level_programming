acf05720c783d636831344c649fb9060e34971cf
Code Slayer <3613@holbertonschool.com>
2022-04-05 13:40:30 -0500

    used c variable

--- 0x01-javascript_objects_scopes_closures/6-square.js
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -7,13 +7,12 @@ class Square extends Rectangle {
   }
 
   charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        if (c === undefined) {
-          process.stdout.write('X');
-        } else {
-          process.stdout.write('C');
-        }
+        process.stdout.write(c);
       }
       process.stdout.write('\n');
     }
